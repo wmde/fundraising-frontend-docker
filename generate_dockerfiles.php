aa12b0185953cc38f022a7ea38e0cd432bda7244
Corinna Hillebrand <corinna.hillebrand@wikimedia.de>
2020-05-20 15:40:28 +0200

    Update minor versions on packages

--- generate_dockerfiles.php
+++ generate_dockerfiles.php
@@ -2,9 +2,9 @@
 <?php
 
 $PHP_VERSION = "7.4";
-$XDEBUG_VERSION = "2.9.4";
+$XDEBUG_VERSION = "2.9.5";
 $KONTOCHECK_VERSION = "6.11";
-$COMPOSER_VERSION = "1.10.5";
+$COMPOSER_VERSION = "1.10.6";
 $PHPSTAN_VERSION = "^0.12";
 
 $headerTemplate = <<<HD
